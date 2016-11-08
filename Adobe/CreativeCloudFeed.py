c3aca3d4520a0869095286af744714b6ff200e03
mosen <mosen1@gmail.com>
2016-11-08 22:13:00 +1100

    Changed input variables in feed provider to allow for multiple platforms or channels. Added more output variables matching their relevant munki variable names. Build request URL dynamically. Update Photoshop recipe to indicate correct base version.

--- Adobe/CreativeCloudFeed.py
+++ Adobe/CreativeCloudFeed.py
@@ -1,27 +1,53 @@
 #!/usr/bin/python
+#
+# Copyright 2016 Mosen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import string
 import json
 import urllib2
+from urllib import urlencode
+from distutils.version import LooseVersion as LV
 
 from autopkglib import Processor, ProcessorError
 
 __all__ = ["CreativeCloudFeed"]
 
-URL = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v4/products/all?&channel=ccm&channel=sti&platform=osx10&platform=osx10-64&payload=true&productType=Desktop&_type=json'
+BASE_URL = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v4/products/all'
+# URL = 'https://prod-rel-ffc-ccm.oobesaas.adobe.com/adobe-ffc-external/core/v4/products/all?&channel=ccm&channel=sti&platform=osx10&platform=osx10-64&payload=true&productType=Desktop&_type=json'
 
 class CreativeCloudFeed(Processor):
     """Fetch information about product(s) from the Creative Cloud products feed."""
-
+    description = __doc__
     input_variables = {
-        "platform_id": {
-            "default": "osx10-64",
-            "description": "The deployment platform, defaults to osx10-64."
-        },
         "product_id": {
+            "required": True,
             "description": "The product sap code"
         },
         "base_version": {
+            "required": True,
             "description": "The base product version"
+        },
+        "channels": {
+            "required": False,
+            "default": "ccm",
+            "description": "The update feed channel(s), comma separated. (default is ccm)"
+        },
+        "platforms": {
+            "required": False,
+            "default": "osx10,osx10-64",
+            "desription": "The deployment platform(s), comma separated. (default is osx10,osx10-64)"
         }
     }
 
@@ -30,37 +56,103 @@ class CreativeCloudFeed(Processor):
             "description": "Product main information URL"
         },
         "icon_url": {
-            "description": "Icon download URL"
+            "description": "Icon download URL for the highest resolution available, normally 96x96."
         },
         "base_version": {
             "description": "The basic (major.minor) version"
         },
-        "product_version": {
+        "version": {
             "description": "The full length version"
+        },
+        "display_name": {
+            "description": "The product full name and major version"
+        },
+        "manifest_url": {
+            "description": "The URL to the product manifest"
+        },
+        "family": {
+            "description": "The product family"
+        },
+        "minimum_os_version": {
+            "description": "The minimum operating system version required to install this package"
         }
     }
 
-    def main(self):
-        product_id = self.get('product_id')
-        base_version = self.get('base_version')
+    def feed_url(self, channels, platforms):
+        """Build the GET query parameters for the product feed."""
+        params = [
+            ('payload', 'true'),
+            ('productType', 'Desktop'),
+            ('_type', 'json')
+        ]
+        for ch in channels:
+            params.append(('channel', ch))
 
-        req = urllib2.Request(URL, headers={
+        for pl in platforms:
+            params.append(('platform', pl))
+
+        return BASE_URL + '?' + urlencode(params)
+
+    def main(self):
+        product_id = self.env.get('product_id')
+        base_version = self.env.get('base_version')
+        channels = string.split(self.env.get('channels'), ',')
+        platforms = string.split(self.env.get('platforms'), ',')
+
+        url = self.feed_url(channels, platforms)
+        self.output('Fetching from feed URL: {}'.format(url))
+
+        req = urllib2.Request(url, headers={
             'User-Agent': 'Creative Cloud',
             'x-adobe-app-id': 'AUSST_4_0',
         })
         data = json.loads(urllib2.urlopen(req).read())
-        ccm_data = {}
+        channel_data = {}
         for channel in data['channel']:
-            if channel['name'] == 'ccm':
-                ccm_data.update(channel)
+            if channel['name'] in channels:
+                channel_data[channel['name']] = channel
 
-        matching_prods = []
-        for prod in ccm_data['products']['product']:
-            if prod['id'] != product_id:
+        product = {'version': '1.0'}
+        for channel in data['channel']:
+            if channel['name'] not in channels:
                 continue
 
-            if prod['platforms']['platform'][0]['languageSet'][0].get('baseVersion') == base_version:
-                matching_prods.append(prod)
+            for prod in channel['products']['product']:
+                if prod['id'] != product_id:
+                    continue
+
+                if prod['platforms']['platform'][0]['languageSet'][0].get('baseVersion') != base_version:
+                    continue
+
+                if 'version' not in prod:
+                    self.output('product has no version: {}'.format(prod['displayName']))
+                    continue
+
+                #  self.output('check if version: {} is greater than newest found product: {}'.format(prod['version'], product['version']))
+                if LV(prod['version']) > LV(product['version']):
+                    product = prod
+
+        first_platform = {}
+        for platform in product['platforms']['platform']:
+            if platform['id'] in platforms:
+                first_platform = platform
+                break
+
+        if first_platform.get('packageType') == 'RIBS':
+            raise ProcessorError('This process does not support RIBS style packages.')
+
+        self.output('Found matching product {}, version: {}'.format(product.get('displayName'), product.get('version')))
+
+        compatibility_range = first_platform['systemCompatibility']['operatingSystem']['range'][0]
+
+        # output variable naming has been kept as close to pkginfo names as possible in order to feed munkiimport
+        self.env['minimum_os_version'] = compatibility_range.split('-')[0]
+        self.env['product_info_url'] = product.get('productInfoPage')
+        self.env['version'] = product.get('version')
+        self.env['display_name'] = product.get('displayName')
+
+
+
 
 
 if __name__ == "__main__":
